15ac9748710c7f073af82d294c7d28e6480b2f86
danielhammerl <daniel.hammerl99@gmx.de>
2024-08-11 14:05:44 +0200

    update

--- update-and-run.sh
+++ update-and-run.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-git fetch && git pull && ./compile.sh && ./run.sh
+
+git fetch
+
+output=$(git pull)
+
+if [[ $output != *"Already up to date."* ]] || [[ ! -f "ksp-controller" ]]; then
+    ./compile.sh
+fi
+
+./run.sh
