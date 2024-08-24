f35bd8b4a6e9c9968c7e6414a382dd231a94ecc1
danielhammerl <daniel.hammerl99@gmx.de>
2024-08-24 13:39:23 +0200

    changed systemd

--- src/main.cpp
+++ src/main.cpp
@@ -63,8 +63,8 @@ bool actionGroupsPressed[10] = {false, false, false, false, false, false, false,
     mcp23017Setup(100, 0x20);
 
     pinMode(100, OUTPUT);
-    pinMode(107, INPUT);
     pinMode(106, INPUT);
+    pinMode(105, INPUT);
 
     // joystick has to be js0, means if multiple joysticks are connected only one works
     auto joystickDevice = open("/dev/input/js0", O_RDONLY | O_NONBLOCK);
@@ -131,7 +131,7 @@ bool actionGroupsPressed[10] = {false, false, false, false, false, false, false,
 
             digitalWrite(100, lights ? HIGH : LOW);
 
-            if (digitalRead(107) == HIGH) {
+            if (digitalRead(106) == HIGH) {
                 if(!actionGroupsPressed[0]) {
                     actionGroupsPressed[0] = true;
                     vessel.control().toggle_action_group(0);
@@ -141,7 +141,7 @@ bool actionGroupsPressed[10] = {false, false, false, false, false, false, false,
                 actionGroupsPressed[0] = false;
             }
 
-            if (digitalRead(106) == HIGH) {
+            if (digitalRead(105) == HIGH) {
                 if(!actionGroupsPressed[1]) {
                     actionGroupsPressed[1] = true;
                     vessel.control().toggle_action_group(1);
