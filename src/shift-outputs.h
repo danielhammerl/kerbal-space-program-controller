495b1b5b9ce9a04fe10606bb61f8948ea3cf0ea0
danielhammerl <mail@danielhammerl.de>
2024-12-29 13:05:49 +0100

    timings

--- src/shift-outputs.h
+++ src/shift-outputs.h
@@ -39,7 +39,7 @@ public:
     };
 
     void outputData(const ShiftOutputData &data) {
-        if (timer.elapsedMilliseconds() > 1500) {
+        if (timer.elapsedMilliseconds() > 250) {
             // third bit shifter, 7 to 0
             storeBitInRegister(data.highG);
             storeBitInRegister(data.heatShieldWarning);
