1897a74a2b498c06bc169b7f78f16989896ae76c
danielhammerl <mail@danielhammerl.de>
2025-08-23 22:40:27 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <vector>
 #include "boost/asio.hpp"
 
 int main() {
-    boost::asio::io_context io_context;
-    boost::asio::ip::tcp::acceptor acceptor(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 12345));
+    std::vector<unsigned short> ports = {12800, 12801, 12802, 12803};
+    std::vector<boost::asio::io_context> io_contexts(ports.size());
+    std::vector<boost::asio::ip::tcp::endpoint> endpoints;
+    std::vector<boost::asio::ip::tcp::acceptor> acceptors;
+    std::vector<boost::asio::ip::tcp::socket> sockets;
+
+    for (size_t i = 0; i < ports.size(); ++i) {
+        endpoints.emplace_back(boost::asio::ip::tcp::v4(), ports[i]);
+        acceptors.emplace_back(io_contexts[i], endpoints[i]);
+        sockets.emplace_back(io_contexts[i]);
+    }
 
     std::cout << "KSP Controller started" << std::endl;
+    std::cout << "Opened tcp ports for subsystems on: ";
+    for (size_t i = 0; i < ports.size(); ++i) {
+        std::cout << endpoints[i].port();
+        if (i < ports.size() - 1) std::cout << ", ";
+    }
+    std::cout << std::endl;
+
+    /*for (size_t i = 0; i < ports.size(); ++i) {
+        acceptors[i].accept(sockets[i]);
+    }*/
 
     return 0;
 }
