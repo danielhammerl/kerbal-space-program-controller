bdb630a121a883470b7840b4bd3701a5c40239fb
danielhammerl <mail@danielhammerl.de>
2025-08-23 19:44:49 +0200

    boost stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "boost/asio.hpp"
 
 int main() {
+    boost::asio::io_context io_context;
+    boost::asio::ip::tcp::acceptor acceptor(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 12345));
+
     std::cout << "KSP Controller started" << std::endl;
 
     return 0;
